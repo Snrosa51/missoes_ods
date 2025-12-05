5cd104d85235eb7108dc2b49ae68f162e0afbe83
Snrosa51 <190318640+Snrosa51@users.noreply.github.com>
2025-12-05 15:31:23 -0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,28 +1,17 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-// Se existir DATABASE_URL (Railway), usa ela.
-// Caso contrário, usa as variáveis locais do .env
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'mysql',
-      logging: false,
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      }
-    })
-  new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql',
-        logging: false,
-      }
-    );
+// Railway MYSQL_* variables
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE,      // database
+  process.env.MYSQLUSER,          // username
+  process.env.MYSQLPASSWORD,      // password
+  {
+    host: process.env.MYSQLHOST,  // host (private network)
+    port: process.env.MYSQLPORT,  // 3306 usually
+    dialect: 'mysql',
+    logging: false
+  }
+);
 
 module.exports = sequelize;
