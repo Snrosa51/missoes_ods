d750fc671b22ad59d49325e6bf312eb2248e53b7
SandroNR <snrosa.ingles@gmail.comm>
2025-12-07 01:43:44 -0300

    Fix DB.JS

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,27 +1,20 @@
-// backend/config/db.js
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// 🔥 Garantir que DATABASE_URL existe
 if (!process.env.DATABASE_URL) {
   console.error("❌ ERRO: DATABASE_URL não está definida!");
   process.exit(1);
 }
-const dbURL = "mysql://user:password@host:port/database"
-DATABASE_URL = dbURL
 
-console.log("🔍 DATABASE_URL carregada:", process.env.DATABASE_URL);
-
-// Conexão única para Railway
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "mysql",
   logging: false,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,
-    },
-  },
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
